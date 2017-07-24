61f06f67e0736dc13370dc96320d961fb3f812bf
Dhiren Patel <dhirenspatel@hotmail.com>
2017-07-25 01:15:21 +0530

    Add project files.

---
+++ ExceptionHandlingInMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExceptionHandlingInMVC.Startup))]
+namespace ExceptionHandlingInMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
